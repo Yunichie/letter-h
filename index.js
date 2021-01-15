06e8d5f2abd2c9323807e60718ba84133a048a4a
Yunichi <alfarelsandriano@gmail.com>
2021-01-15 01:43:39 +0000

    h-ed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = function h(string) {
   if(typeof(string) !== "string") throw new TypeError("argument(s) must be a string!");
-  return string.replace("h");
+  return string.replace(/\S/g, "h");
 }
