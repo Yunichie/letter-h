58a552ee284f7d1d6f560f0eb3ec7d3434afec79
Yunichi <alfarelsandriano@gmail.com>
2021-01-16 10:21:03 +0000

    opt

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 module.exports = function h(string) {
+  if(string.size === "big") {
+    return string.replace(/\S/g, "H");
+  } else {
+    if(string.size === "small") {
+      return string.replace(/\S/g, "h");
+    }
+  }
+  
   if(typeof(string) !== "string") throw new TypeError("argument(s) must be a string!");
   return string.replace(/\S/g, "h");
 }
